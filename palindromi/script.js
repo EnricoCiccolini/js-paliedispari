2c70a01ca8fde7cf52872af9101fcdd59d7cc7c1
EnricoCiccolini <piroroma90@gmail.com>
2025-02-28 15:14:07 +0100

    feat : palindrome complete

--- palindromi/script.js
+++ palindromi/script.js
@@ -3,4 +3,42 @@
 //Creare una funzione per capire se la parola inserita è palindroma
 
 
+function isPalindrome(word) {
+    let word1
+    let word2 = ('')
+    let palindrome
+
+    word1 = word.split('')
+    console.log(word1)
+    for (let i = word1.length - 1; i >= 0; i--) {
+
+        word2 += word1[i]
+    }
+    console.log(word2)
+
+    palindrome = word2 === word
+
+    console.log(palindrome)
+    return palindrome = word2 === word
+}
+
+
+let wordRequest = prompt('inserisci la parola da controllare')
+let check = isPalindrome(wordRequest)
+console.log(check)
+
+if (check) {
+    console.log(' la parola è palindroma')
+}
+else  {
+   console.log (' la parola non è palindroma')
+}
+
+
+
+
+
+
+
+
 
